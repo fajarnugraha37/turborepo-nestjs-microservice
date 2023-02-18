b2c312c421b4bfd848934d6576cc306e36d6fb77
fajar <nugrahafajar37@gmail.com>
2023-02-18 20:55:49 +0700

    [global-config] add type definiton

---
+++ packages/global-config/index.d.ts
@@ -0,0 +1,4 @@
+declare function loadConfig(): Record<string, any>;
+declare const config: Record<string, any>;
+export { loadConfig, config, };
+//# sourceMappingURL=index.d.ts.map
---
+++ packages/global-config/index.js
@@ -0,0 +1,37 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+exports.__esModule = true;
+exports.config = exports.loadConfig = void 0;
+var fs_1 = require("fs");
+var yaml = __importStar(require("js-yaml"));
+var path_1 = require("path");
+var YAML_CONFIG_FILENAME = 'config.yaml';
+function loadConfig() {
+    return yaml.load((0, fs_1.readFileSync)((0, path_1.join)(__dirname, '..', '..', YAML_CONFIG_FILENAME), 'utf8'));
+}
+exports.loadConfig = loadConfig;
+;
+var config = loadConfig();
+exports.config = config;
